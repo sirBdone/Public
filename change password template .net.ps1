738af93da3289f6b6c432b57b8b306de8ee2d414
Don B Ran <iamarandomstudent@gmail.com>
2023-11-04 03:10:41 -0400

    good upgrade. saved

--- change password template .net.ps1
+++ change password template .net.ps1
@@ -3,7 +3,13 @@
 # Create a form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Change Password: length:16-32 Aa1[!@#]"
-$form.Size = New-Object System.Drawing.Size(300, 200)
+$form.Size = New-Object System.Drawing.Size(450, 200)
+$form.MinimumSize = New-Object System.Drawing.Size(450, 200)
+$form.MaximumSize = New-Object System.Drawing.Size(600, 200)
+
+# Disable the ability to resize in the height direction
+#$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+
 $form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
 
 # Create labels, textboxes, and buttons
@@ -14,6 +20,8 @@ $form.Controls.Add($label1)
 
 $textUsername = New-Object System.Windows.Forms.TextBox
 $textUsername.Location = New-Object System.Drawing.Point(120, 20)
+$textUsername.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+$textUsername.Width = 300
 $form.Controls.Add($textUsername)
 
 $label2 = New-Object System.Windows.Forms.Label
@@ -21,57 +29,94 @@ $label2.Location = New-Object System.Drawing.Point(20, 50)
 $label2.Text = "Password:"
 $form.Controls.Add($label2)
 
-$textPassword = New-Object System.Windows.Forms.TextBox
-$textPassword.Location = New-Object System.Drawing.Point(120, 50)
-$form.Controls.Add($textPassword)
-$textPassword.PasswordChar = '*'
+$textPasswordSecure = New-Object System.Windows.Forms.TextBox
+$textPasswordSecure.Location = New-Object System.Drawing.Point(120, 50)
+$textPasswordSecure.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+$textPasswordSecure.Width = 200
+$textPasswordSecure.PasswordChar = "*"
+$form.Controls.Add($textPasswordSecure)
 
 $label3 = New-Object System.Windows.Forms.Label
 $label3.Location = New-Object System.Drawing.Point(20, 80)
 $label3.Text = "Re-enter Password:"
 $form.Controls.Add($label3)
 
-$textPasswordReentered = New-Object System.Windows.Forms.TextBox
-$textPasswordReentered.Location = New-Object System.Drawing.Point(120, 80)
-$form.Controls.Add($textPasswordReentered)
-$textPasswordReentered.PasswordChar = '*'
+$textPasswordReenteredSecure = New-Object System.Windows.Forms.TextBox
+$textPasswordReenteredSecure.Location = New-Object System.Drawing.Point(120, 80)
+$textPasswordReenteredSecure.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+$textPasswordReenteredSecure.Width = 200
+$textPasswordReenteredSecure.PasswordChar = "*"
+$form.Controls.Add($textPasswordReenteredSecure)
+
+$showTextButton = New-Object System.Windows.Forms.Button
+$showTextButton.Location = New-Object System.Drawing.Point(345, 47)
+$showTextButton.Anchor = [System.Windows.Forms.AnchorStyles]::Top -band [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
+$showTextButton.Text = "Show Text"
+$form.Controls.Add($showTextButton)
+
+$showTextButton.Add_Click({
+    if ($textPasswordSecure.PasswordChar -eq "*") {
+        $textPasswordSecure.PasswordChar = $null
+        #$textPasswordReenteredSecure.PasswordChar = $null
+    } else {
+        $textPasswordSecure.PasswordChar = "*"
+        #$textPasswordReenteredSecure.PasswordChar = "*"
+    }
+})
+
+$showTextButton2 = New-Object System.Windows.Forms.Button
+$showTextButton2.Location = New-Object System.Drawing.Point(345, 78)
+$showTextButton2.Anchor = [System.Windows.Forms.AnchorStyles]::Top -band [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
+$showTextButton2.Text = "Show Text"
+$form.Controls.Add($showTextButton2)
+
+$showTextButton2.Add_Click({
+    if ($textPasswordReenteredSecure.PasswordChar -eq "*") {
+        #$textPasswordSecure.PasswordChar = $null
+        $textPasswordReenteredSecure.PasswordChar = $null
+    } else {
+        #$textPasswordSecure.PasswordChar = "*"
+        $textPasswordReenteredSecure.PasswordChar = "*"
+    }
+})
 
 $button = New-Object System.Windows.Forms.Button
-$button.Location = New-Object System.Drawing.Point(100, 120)
+$button.Location = New-Object System.Drawing.Point(245, 120)
 $button.Text = "Submit"
+$button.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
 $form.Controls.Add($button)
 
 # Add an event handler for the button click
 $button.Add_Click({
     $username = $textUsername.Text
-    $password = $textPassword.Text
-    $passwordReentered = $textPasswordReentered.Text
+    $password = $textPasswordSecure.Text
+    $passwordReentered = $textPasswordReenteredSecure.Text
 
     if ($password -eq $passwordReentered -and $password -match "^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#%^&*(){}|<>?,./\[\]]).{16,32}$") {
         # Password meets the criteria
         # Here, you can make the JSON API call with the $username and $password variables
         # Replace the following line with your API call logic
-        Write-Host "Username: $username, Password: $password (API call would go here)"
+        Write-Host "Username: $username"
+        write-host "Password: $password"
+        # (API call would go here)"
         $form.DialogResult = [System.Windows.Forms.DialogResult]::OK
     } else {
         [System.Windows.Forms.MessageBox]::Show("Password does not meet the criteria. Please use 16-32 characters, 1 uppercase letter, 1 lowercase letter, and one of these '!@#$'. ")
     }
 })
 
-# Create a Cancel button
 $cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Location = New-Object System.Drawing.Point(200, 120)
+$cancelButton.Location = New-Object System.Drawing.Point(345, 120)
+$cancelButton.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
 $cancelButton.Text = "Cancel"
 $form.Controls.Add($cancelButton)
 
-# Add an event handler for the Cancel button
 $cancelButton.Add_Click({
     $form.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
 })
 
 $form.Topmost = $True
 
-# Display the form using ShowDialog to make it modal
 $result = $form.ShowDialog()
 
 if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
