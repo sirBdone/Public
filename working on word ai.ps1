ca281ef668ed8876d4cc638a60cdb273cda1614a
sirBdone <67079134+sirBdone@users.noreply.github.com>
2023-12-08 21:59:08 -0500

    Write-toWord template
    This creates a word document with the markdown. Change saveas link, change markdown to your liking.

---
+++ working on word ai.ps1
@@ -0,0 +1,22 @@
+﻿$word = New-Object -ComObject word.application
+$document = $word.documents.add()
+
+$markdown = @"
+# Sample Markdown
+
+## Heading 2
+
+This is a paragraph with **bold text** and *italicized text*.
+
+### Heading 3
+
+- This is a bullet point.
+- This is another bullet point.
+"@
+
+$selection = $word.selection
+$selection.typetext($markdown)
+
+$document.saveas("C:\Users\sirbd0ne\Documents\$($prompt.split("\.")[1]).docx")
+$document.close()
+$word.quit()
